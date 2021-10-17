1fbb574d52b41bab2115a9cd3e2ea167a6249398
Andrew <Dimedrov123@gmail.com>
2021-10-17 18:02:23 +0300

    template events added

--- API/Includes/NII/Utility/Event.hpp
+++ API/Includes/NII/Utility/Event.hpp
@@ -11,67 +11,72 @@
 namespace nii::util
 {
 
+    template<class ... ARGS>
     class Event;
+    template<class ... ARGS>
     class Listener;
 
     namespace base
     {
+        template<class ... ARGS>
+        class Listener;
 
-        class BaseEvent;
-        class BaseListener;
-
-        class BaseEvent
+        template<class ... ARGS>
+        class Event
         {
-            friend BaseListener; 
-            friend Listener;
+            friend nii::util::Listener<ARGS...>; 
+            friend Listener<ARGS...>;
 
         protected:
-            virtual void unbind(BaseListener*) =0;
-            virtual void bind(BaseListener*) =0;
-            void connect(BaseListener *listener);
+            virtual void unbind(Listener<ARGS...> *) =0;
+            virtual void bind(Listener<ARGS...> *) =0;
+            void connect(Listener<ARGS...>  *listener);
         };
 
-        class BaseListener
+        template<class ... ARGS>
+        class Listener
         {
-            friend BaseEvent;
-            friend Event;
+            friend nii::util::Event<ARGS...>;
+            friend Event<ARGS...>;
 
         protected:
-            virtual void unbind(BaseEvent*) =0;
-            virtual void bind(BaseEvent*) =0;
-            void connect(BaseEvent *event);
+            virtual void unbind(Event<ARGS...> *) =0;
+            virtual void bind(Event<ARGS...> *) =0;
+            void connect(Event<ARGS...>  *event);
 
         public:
-            virtual void operator()() =0;
+            virtual void takeCall(ARGS const&...) =0;
         };
 
 
-        inline void BaseEvent::connect(BaseListener *listener)
+        template<class ... ARGS>
+        inline void Event<ARGS...>::connect(Listener<ARGS...> *listener)
         {
             this->bind(listener);
             listener->bind(this);
         }
 
-        inline void BaseListener::connect(BaseEvent *event)
+        template<class ... ARGS>
+        inline void Listener<ARGS...>::connect(Event<ARGS...> *event)
         {
             this->bind(event);
             event->bind(this);
         }
-
         
     } // namespace base
 
     
 
 
-// template<class ... ARGS>
-    class Event : public base::BaseEvent
+    template<class ... ARGS>
+    class Event : public base::Event<ARGS...>
     {
-    using Function  = std::function<void()>;
-    using Listener  = base::BaseListener*;
+    using BaseListener = base::Listener<ARGS...>;
+    using Function  = std::function<void(ARGS...)>;
+    using Listener  = BaseListener*;
     using Variant   = std::variant<Function, Listener>;
     using Container = std::list<Variant>;
-    using Iterator = Container::iterator;
+    using Iterator  = typename Container::iterator;
 
     public:
         class ListenerHandler
@@ -96,86 +101,87 @@ namespace nii::util
 
     public:
         Event();
-        Event(const Event& other);
+        Event(const Event& other) = delete;
         Event(Event&& other);
         ~Event();
 
-        ListenerHandler listenGuard(Function calleable);
-        void listen(Function calleable);
-        void listen(base::BaseListener &listener);
+        ListenerHandler addListenerGuard(Function calleable);
+        void addListener(Function calleable);
+        void addListener(BaseListener &listener);
         inline void removeListener(ListenerHandler& handler);
 
-        void call();
+        void call(const ARGS &... arguments);
+        void operator()(const ARGS &... arguments);
 
      private:
-        void unbind(base::BaseListener *listener) override;
-        void bind(base::BaseListener *listener) override;
+        void unbind(BaseListener *listener) override;
+        void bind(BaseListener *listener) override;
 
      private:
         Container listeners;
     };
 
 
-
-    class Listener : public base::BaseListener
+    template<class ... ARGS>
+    class Listener : public base::Listener<ARGS...>
     {
-    using Container = std::vector<base::BaseEvent*>;
-    
+    using BaseEvent = base::Event<ARGS...>;
+    using Container = std::vector<BaseEvent*>;
+
     public:
         Listener();
         Listener(const Listener& other) = delete;
         Listener(Listener&& other);
         ~Listener();
 
-        void listen(base::BaseEvent &event);
-        void operator()() override;
+        void listen(BaseEvent &event);
+        void takeCall(const ARGS &... arguments) = 0;
 
     private:
-        void unbind(base::BaseEvent *event) override;
-        void bind(base::BaseEvent *event) override;
+        void unbind(BaseEvent *event) override;
+        void bind(BaseEvent *event) override;
 
     private:
         Container events;
     };
 
-    struct AnonymousListener : Listener
+    template<class ... ARGS>
+    struct AnonymousListener : Listener<ARGS...>
     {
+    using Function = std::function<void(ARGS...)>;
 
-        AnonymousListener(std::function<void()> function)
+        AnonymousListener(Function function)
             : function(function)
         {}
         
-        void operator()()
-        {
-            function();
-        }
+        inline void takeCall(ARGS const&... arguments) { function(arguments...); }
 
     private:
-        std::function<void()> function;
+        Function function;
     };
 
+    template<class ... ARGS>
+    AnonymousListener(void(ARGS...)) -> AnonymousListener<ARGS...>;
 
     // ============================== Event ============================== //
 
 
-        inline Event::Event()
+        template<class ... ARGS>
+        inline Event<ARGS...>::Event()
             : listeners()
         {
             std::cout << "EVENT" << std::endl;
         }
 
-        inline Event::Event(const Event& other)
-            : listeners(other.listeners)
-        {
-            std::cout << "EVENT cp" << std::endl;
-        }
-        inline Event::Event(Event&& other)
+        template<class ... ARGS>
+        inline Event<ARGS...>::Event(Event&& other)
             : listeners(std::move(other.listeners))
         {
             std::cout << "EVENT mv" << std::endl;
         }
 
-        inline Event::~Event()
+        template<class ... ARGS>
+        inline Event<ARGS...>::~Event()
         {
             std::cout << "~EVENT" << std::endl;
             for(auto &listener : listeners) {
@@ -186,24 +192,28 @@ namespace nii::util
             }
         }
 
-        inline Event::ListenerHandler Event::listenGuard(Event::Function calleable)
+        template<class ... ARGS>
+        inline typename Event<ARGS...>::ListenerHandler Event<ARGS...>::addListenerGuard(Function calleable)
         {
             auto iterator = listeners.emplace(listeners.end(), Variant{std::move(calleable)});
             return ListenerHandler(this, std::move(iterator));
         }
 
-        inline void Event::listen(Event::Function calleable)
+        template<class ... ARGS>
+        inline void Event<ARGS...>::addListener(Function calleable)
         {
             listeners.emplace_back(std::move(calleable));
         }
 
-        void Event::listen(base::BaseListener &listener)
+        template<class ... ARGS>
+        inline void Event<ARGS...>::addListener(BaseListener &listener)
         {
-            connect(&listener);
+            this->connect(&listener);
         }
 
 
-        inline void Event::removeListener(ListenerHandler& handler)
+        template<class ... ARGS>
+        inline void Event<ARGS...>::removeListener(ListenerHandler& handler)
         {
             if(handler.event == nullptr) {
                 throw "Can't unbind empty listener handler!"; //TODO logical error
@@ -214,20 +224,28 @@ namespace nii::util
             listeners.erase(handler.iterator);
         }
 
-        inline void Event::call()
+        template<class ... ARGS>
+        inline void Event<ARGS...>::call(const ARGS &... arguments)
         {
             for(auto &callable : listeners) {
-                std::visit([](auto && arg) { 
+                std::visit([&arguments...](auto && arg) { 
                     using T = std::decay_t<decltype(arg)>;
                     if constexpr (std::is_same_v<T, Function>)
-                        arg();
+                        arg(arguments...);
                     else if constexpr (std::is_same_v<T, Listener>)
-                        arg->operator()();
+                        arg->takeCall(arguments...);
                 }, callable);
             }
         }
 
-        inline void Event::unbind(base::BaseListener *listener)
+        template<class ... ARGS>
+        inline void Event<ARGS...>::operator()(const ARGS &... arguments)
+        {
+            call(arguments...);
+        }
+
+        template<class ... ARGS>
+        inline void Event<ARGS...>::unbind(BaseListener *listener)
         {
             for(auto it = listeners.begin(); it != listeners.end(); ++it) {
                 auto ptr = std::get_if<Listener>(&*it);
@@ -238,7 +256,8 @@ namespace nii::util
             }
         }
 
-        inline void Event::bind(base::BaseListener *listener)
+        template<class ... ARGS>
+        inline void Event<ARGS...>::bind(BaseListener *listener)
         {
             listeners.emplace_back(listener);
         }
@@ -246,21 +265,24 @@ namespace nii::util
 
     // ============================== Event::ListenerHandler ============================== //
 
-        inline Event::ListenerHandler::ListenerHandler(Event *event, Event::Iterator iterator)
+        template<class ... ARGS>
+        inline Event<ARGS...>::ListenerHandler::ListenerHandler(Event<ARGS...> *event, Event<ARGS...>::Iterator iterator)
             : event(event)
             , iterator(iterator)
         { 
             std::cout << "LISTENER HAND" << std::endl; 
         }
 
-        inline Event::ListenerHandler::ListenerHandler(Event::ListenerHandler&&other)
+        template<class ... ARGS>
+        inline Event<ARGS...>::ListenerHandler::ListenerHandler(Event<ARGS...>::ListenerHandler&&other)
             : event(std::move(other.event))
             , iterator(std::move(other.iterator))
         {
             other.event = nullptr;
         }
         
-        inline Event::ListenerHandler::~ListenerHandler()
+                template<class ... ARGS>
+        inline Event<ARGS...>::ListenerHandler::~ListenerHandler()
         {
             std::cout << "~LISTENER HAND" << std::endl;
             if(event != nullptr) {
@@ -268,13 +290,15 @@ namespace nii::util
             }
         }
 
-        inline void Event::ListenerHandler::unbind()
+        template<class ... ARGS>
+        inline void Event<ARGS...>::ListenerHandler::unbind()
         {
             event->removeListener(*this);
             event = nullptr;
         }
 
-        inline void Event::ListenerHandler::save()
+        template<class ... ARGS>
+        inline void Event<ARGS...>::ListenerHandler::save()
         {
             event = nullptr;
         }
@@ -283,25 +307,22 @@ namespace nii::util
 
 // ============================== Listener ============================== //
 
-        inline Listener::Listener()
+        template<class ... ARGS>
+        inline Listener<ARGS...>::Listener()
             : events()
         {
             std::cout << "LISTENER" << std::endl;
         }
 
-        // inline Listener::Listener(const Listener& other)
-        //     : events(other.events)
-        // {
-        //     std::cout << "LISTENER cp" << std::endl;
-        // }
-
-        inline Listener::Listener(Listener&& other)
+        template<class ... ARGS>
+        inline Listener<ARGS...>::Listener(Listener&& other)
             : events(std::move(other.events))
         {
             std::cout << "LISTENER mv" << std::endl;
         }
 
-        inline Listener::~Listener()
+        template<class ... ARGS>
+        inline Listener<ARGS...>::~Listener()
         {
             std::cout << "~LISTENER" << std::endl;
             for(auto i : events) {
@@ -309,17 +330,22 @@ namespace nii::util
             }
         }
 
-        inline void Listener::listen(base::BaseEvent &event)
+        template<class ... ARGS>
+        inline void Listener<ARGS...>::listen(BaseEvent &event)
         {
-            connect(&event);
+            this->connect(&event);
         }
 
-        inline void Listener::operator()()
-        {
-            std::cout << "LISTENER ()" << std::endl;
-        }
+        // template<class ... ARGS>
+        // inline void Listener<ARGS...>::takeCall(const ARGS &... arguments)
+        // {
+        //     std::cout << "LISTENER () " ;
+        //     (std::cout << ... << arguments);
+        //     std::cout <<  std::endl;
+        // }
 
-        inline void Listener::unbind(base::BaseEvent *event)
+        template<class ... ARGS>
+        inline void Listener<ARGS...>::unbind(BaseEvent *event)
         {
             for(auto it = events.begin(); it != events.end(); ++it) {
                 if(*it == event) {
@@ -329,7 +355,8 @@ namespace nii::util
             }
         }
 
-        inline void Listener::bind(base::BaseEvent *event)
+        template<class ... ARGS>
+        inline void Listener<ARGS...>::bind(BaseEvent *event)
         {
             events.emplace_back(event);
         }
